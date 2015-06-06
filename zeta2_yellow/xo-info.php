99e5302001c33425f6f709ca373e482364e4f5fd
mambax7 <mambax7@gmail.com>
2015-06-06 10:12:49 -0400

    Original files

---
+++ zeta2_yellow/xo-info.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    // Types of language constants
+    "languages"         => array("main", "admin"),
+
+    "copyright"         => "&copy; Created by burning, maintained by Xoops france <http://www.frxoops.org>",
+    "name"              => "",
+    "description"       => "XOOPS 2.0.1x and 2.3.x, 1/2 or 3 columns, full screen",
+    "version"           => "",
+    "format"            => "Standard / Morpho / Full div",
+    "author"            => "",
+    "demo"              => "",
+    "url"               => "",
+    "download"          => "",
+    "W3C"               => "Xhtml 1.0 Transitional - Css 1/2/3",
+    "licence"           => "GPL",
+    "thumbnail"         => "themedirname.gif",
+    "screenshot"        => "themedirname.jpg",
+);
+?>
+
